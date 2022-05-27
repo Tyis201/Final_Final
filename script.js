6a292a46777756d8e4d24d441198927424d3ddda
tyisyustat24 <tyis.yustat24@icloud.com>
2022-05-27 09:22:03 -0700

    nate

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+console.log('hello world');
+
+const storygame = document.getElementById('story')
+
+
+
+
+
+
+function start(){
+   const game = document.createElement("p");
+   const gametext = document.createTextNode("tyis is gay")
+   node.appendchild(textnode);
+   document.getElementById("story").appendChild(game);
+}
